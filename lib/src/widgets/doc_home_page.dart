a489d7b2458bd97a1c4cc76ff311ad716f906e98
sidneyaguirre <sidney.aguirrec@gmail.com>
2023-08-21 19:56:09 -0500

    shorthand: destructure a record into a local variable

--- lib/src/widgets/doc_home_page.dart
+++ lib/src/widgets/doc_home_page.dart
@@ -11,22 +11,12 @@ class DocHomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    //this pattern [(title, modified: modified)] contains 2 variables that match against the fields of the record
-    //returned by [metadata]
-    final (title, modified: modified) = document.metadata;
-    //because they match, the variable declaration pattern destructures the expression,
-    //accessng its values and binding them to new local variables of the same names and types
-    //[String title] and [Datetime modified]
-
-    var record = (named: 'v', 'y', named2: 'x', 'z');
-    print(record.$1); // 'y'
-    print(record.$2); // 'z'
+    //shorthand for when the name of the pattern is the same as in the record
+    final (title, :modified) = document.metadata;
 
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
-        //To get positional fields use getter $<num> on the record
-        //This returns only unnamed fields:
         title: Text(title),
       ),
       body: Column(
