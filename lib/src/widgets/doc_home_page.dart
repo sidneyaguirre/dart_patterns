2d38583ffe9403ab44aefc608eb0f534f548541c
sidneyaguirre <sidney.aguirrec@gmail.com>
2023-08-22 20:37:41 -0500

    use object pattern

--- lib/src/widgets/doc_home_page.dart
+++ lib/src/widgets/doc_home_page.dart
@@ -10,10 +10,32 @@ class DocHomeScreen extends StatelessWidget {
 
   final Document document;
 
+  String formatDate(DateTime dateTime) {
+    final today = DateTime.now();
+    final difference = dateTime.difference(today);
+
+    return switch (difference) {
+      Duration(inDays: 0) => 'today',
+      Duration(inDays: 1) => 'tomorrow',
+      Duration(inDays: -1) => 'yesterday',
+      //guard clause -> uses the [when] keyword after a case pattern
+      //guard clauses only add a condition to a pattern after is matched
+      //if the guard clause is false, the entire pattern is refuted and execution proceeds to the next case
+      Duration(inDays: final days) when days > 7 =>
+        '${days ~/ 7} weeks from now',
+      Duration(inDays: final days) when days < -7 =>
+        '${days.abs() ~/ 7} weeks ago',
+      Duration(inDays: final days, isNegative: true) =>
+        '${days.abs()} days ago',
+      Duration(inDays: final days) => '$days days from now',
+    };
+  }
+
   @override
   Widget build(BuildContext context) {
     //shorthand for when the name of the pattern is the same as in the record
     final (title, :modified) = document.metadata;
+    final formattedModifiedDate = formatDate(modified);
     final blocks = document.getBlocks();
 
     return Scaffold(
@@ -25,7 +47,7 @@ class DocHomeScreen extends StatelessWidget {
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
           Center(
-            child: Text('Last modified $modified'),
+            child: Text('Last modified $formattedModifiedDate'),
           ),
           Expanded(
             child: ListView.builder(
